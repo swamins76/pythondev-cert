e46afba27b9b43630ef3f98c6dca63c289344e70
Swaminathan Shankar Subbu <swaminathans76@gmail.com>
2023-03-22 16:45:51 -0400

    file modified

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-fname = 'Swaminathan'
+import random
+suit = ["Clubs", "Spades", "Hearts", "Diamonds"]
+faces = ["Jack", "Queen", "King", "Ace"]
+numbered = [2,3,4,5,6,7,8,9,10]
 lname = 'Shankar Subbu'
-mylist = ["swami", "shreya"]
-print (f'{fname.upper()} {lname}')
-print ((mylist[1]))
+mylist = ["swami", "shankar", "subbu"]
+#print (f'{fname.upper()} {lname}')
+print ((mylist[2]))
+def draw():
+    which_suit = random.choice(suit)
+    which_type = random.choice([faces, numbered])
+    which_card = random.choice(which_type)
+    return which_card; "of", which_suit
+
+
+print (draw())
+print(draw())
+print(draw())
